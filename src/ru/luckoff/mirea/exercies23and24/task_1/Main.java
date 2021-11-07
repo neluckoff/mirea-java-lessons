d1b42aa721ec86a94d3a61e2244ab6b0b4c8b246
neluckoff <neluckoff@gmail.com>
2021-11-07 14:53:30 +0300

    23-24

---
+++ src/ru/luckoff/mirea/exercies23and24/task_1/Main.java
@@ -0,0 +1,20 @@
+package ru.luckoff.mirea.exercies23and24.task_1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        CSVParser csv = new CSVParser();
+
+        List<String> lines = Files.readAllLines(Paths.get("src/ru/luckoff/mirea/exercies23and24/task_1/movementList.csv"));
+        for (int i = 1; i < lines.size(); i++) {
+            csv.CalculateData(lines.get(i));
+        }
+
+        csv.printCSV();
+    }
+}
---
+++ src/ru/luckoff/mirea/exercies23and24/task_2/HTMLParser.java
@@ -0,0 +1,60 @@
+package ru.luckoff.mirea.exercies23and24.task_2;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HTMLParser {
+
+    private static String IMAGE_DESTINATION_FOLDER = "src/ru/luckoff/mirea/exercies23and24/task_2/images";
+
+    public static void main(String[] args) throws IOException {
+        String strURL = "http://www.mirea.ru";
+        Document document = Jsoup
+                .connect(strURL)
+                .userAgent("Mozilla/5.0")
+                .timeout(10 * 1000)
+                .get();
+
+        Elements imageElements = document.select("img");
+
+        for(Element imageElement : imageElements){
+            String strImageURL = imageElement.attr("abs:src");
+            downloadImage(strImageURL);
+        }
+    }
+
+    private static void downloadImage(String strImageURL){
+        String strImageName =
+                strImageURL.substring( strImageURL.lastIndexOf("/") + 1 );
+
+        System.out.println("Saving: " + strImageName + ", from: " + strImageURL);
+        try {
+            URL urlImage = new URL(strImageURL);
+            InputStream in = urlImage.openStream();
+
+            byte[] buffer = new byte[4096];
+            int n = -1;
+
+            OutputStream os =
+                    new FileOutputStream( IMAGE_DESTINATION_FOLDER + "/" + strImageName );
+
+            while ( (n = in.read(buffer)) != -1 ){
+                os.write(buffer, 0, n);
+            }
+
+            os.close();
+            System.out.println("Image saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
